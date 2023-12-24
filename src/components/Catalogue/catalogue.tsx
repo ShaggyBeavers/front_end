07f3eb50bdc0977915a07cdc615fef8cae1c1d52
yaakymiv <yaryna.yakymiv.shi.2022@lpnu.ua>
2023-12-24 19:42:35 +0200

    started working on filter menu,have some difficulties with styling/logic

--- src/components/Catalogue/catalogue.tsx
+++ src/components/Catalogue/catalogue.tsx
@@ -7,7 +7,9 @@ import SwitchBtn from '../SwitchButton/switch_btn';
 import Pagination from '../Pagination/pagination';
 import NotFound from '../NotFound/not_found';
 import FilterRelicsLine from '../icons/filter_relics_line';
-import agent from '../../app/api/agent';
+import FilterModal from '../FilterModal/filter_modal';
+import FilterExpand from '../icons/filter_expand';
+import SelFilterExpand from '../icons/sel_filter_expand';
 
 interface Photo {
   id: number;
@@ -21,6 +23,9 @@ const Catalogue = () => {
   const [items, setItems] = useState<Photo[]>([]);
   const [notFound, setNotFound] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+  const [selectedFilterOptions, setSelectedFilterOptions] = useState<string[]>([]);
+  const [isFilterModalOpen, setIsFilterModalOpen] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
   const totalPages = 6;//temporarily hardcoded
@@ -59,93 +64,132 @@ const Catalogue = () => {
     }
   };
 
+  const handleFilterCategoryClick = (category: string) => {
+    setSelectedCategory(category === selectedCategory ? null : category);
+    setIsFilterModalOpen(true);
+  };
+
+  const handleFilterOptionClick = (option: string) => {
+    setSelectedFilterOptions((prevOptions) => [...prevOptions, option]);
+  };
+
+  const filterOptions = ['Option1', 'Option2', 'Option3','cow','opposum','Drake'];
   return (<>{
-    notFound ? <NotFound /> : <div className="catalogue-container" >
-      <div className='cat_left'>
-        <div className='cat_filter'>
-          <div className='cat_photo'>
-            <h6>Фото</h6>
-            <SwitchBtn />
-          </div>
-          <div className='filter_categories'>
-            <ul>
-              <li>
-                <FilterRelicsLine />
-                <div>
-                  <h6>Категорія</h6>
-                  <img src="/icons/filter_expand.svg" />
-                </div>
+    notFound ? <NotFound /> :
+      <div> 
+        {/* Filter Modal */}
+          {isFilterModalOpen && (
+            <FilterModal
+              options={filterOptions}
+              selectedOptions={selectedFilterOptions}
+              onClose={() => { setIsFilterModalOpen(false); setSelectedCategory(null); }}
+              onOptionClick={(option) => handleFilterOptionClick(option)}
+            />
+          )}
+        <div className={`catalogue-container ${isFilterModalOpen ? 'dim' : ''}`}>
+         
+          <div className='cat_left'>
+            <div className='cat_filter'>
+              <div className='cat_photo'>
+                <h6>Фото</h6>
+                <SwitchBtn />
+              </div>
+              <div className='filter_categories'>
+                <ul>
+                  <li className={selectedCategory === 'category' ? 'selected' : ''}
+                    onClick={() => handleFilterCategoryClick('category')} style={{marginTop:'0.6rem'}}>
+                    <FilterRelicsLine />
+                    <div className='filter_inner'>
+                      <h6>Категорія</h6>
+                      {selectedCategory === 'category' ? <SelFilterExpand /> : <FilterExpand />}
+                    </div>
+                  </li>
 
-              </li>
-              <li>
-              <FilterRelicsLine />
-                <div>
-                <h6>Місце</h6>
-                <img src="/icons/filter_expand.svg" />
-                </div>
-              </li>
-              <li>
-              <FilterRelicsLine />
-                <div>
-                <h6>Матеріал</h6>
-                <img src="/icons/filter_expand.svg" />
-                </div>
-              </li>
-              <li>
-              <FilterRelicsLine />
-                <div>
-                <h6>Дата створення</h6>
-                <img src="/icons/filter_expand.svg" />
-                </div>
-              </li>
-              <li>
-              <FilterRelicsLine />
-                <div>
-                <h6>Статус</h6>
-                <img src="/icons/filter_expand.svg" />
-                </div>
-              </li>
-              <li>
-              <FilterRelicsLine />
-                <div>
-                <h6>Колекція</h6>
-                <img src="/icons/filter_expand.svg" />
-                </div>
-              </li>
-              <li>
-              <FilterRelicsLine />
-                <div>
-                <h6>Автор</h6>
-                <img src="/icons/filter_expand.svg" />
-                </div>
-              </li>
-              <li>
-              <FilterRelicsLine />
-                <div>
-                <h6>Техніка</h6>
-                <img src="/icons/filter_expand.svg" />
-                </div>
-                <FilterRelicsLine />
-              </li>
-            </ul>
+                  <li className={selectedCategory === 'place' ? 'selected' : ''}
+                    onClick={() => handleFilterCategoryClick('place')}>
+                    <FilterRelicsLine />
+                    <div className='filter_inner'>
+                      <h6>Місце</h6>
+                      {selectedCategory === 'place' ? <SelFilterExpand /> : <FilterExpand />}
+                    </div>
+                  </li>
+
+                  <li className={selectedCategory === 'material' ? 'selected' : ''}
+                    onClick={() => handleFilterCategoryClick('material')}>
+                    <FilterRelicsLine />
+                    <div className='filter_inner'>
+                      <h6>Матеріал</h6>
+                      {selectedCategory === 'material' ? <SelFilterExpand /> : <FilterExpand />}
+                    </div>
+                  </li>
+
+                  <li className={selectedCategory === 'creationDate' ? 'selected' : ''}
+                    onClick={() => handleFilterCategoryClick('creationDate')}>
+                    <FilterRelicsLine />
+                    <div className='filter_inner'>
+                      <h6>Дата створення</h6>
+                      {selectedCategory === 'creationDate' ? <SelFilterExpand /> : <FilterExpand />}
+                    </div>
+                  </li>
+
+                  <li className={selectedCategory === 'status' ? 'selected' : ''}
+                    onClick={() => handleFilterCategoryClick('status')}>
+                    <FilterRelicsLine />
+                    <div className='filter_inner'>
+                      <h6>Статус</h6>
+                      {selectedCategory === 'status' ? <SelFilterExpand /> : <FilterExpand />}
+                    </div>
+                  </li>
+
+                  <li className={selectedCategory === 'collection' ? 'selected' : ''}
+                    onClick={() => handleFilterCategoryClick('collection')}>
+                    <FilterRelicsLine />
+                    <div className='filter_inner'>
+                      <h6>Колекція</h6>
+                      {selectedCategory === 'collection' ? <SelFilterExpand /> : <FilterExpand />}
+                    </div>
+                  </li>
+
+                  <li className={selectedCategory === 'author' ? 'selected' : ''}
+                    onClick={() => handleFilterCategoryClick('author')}>
+                    <FilterRelicsLine />
+                    <div className='filter_inner'>
+                      <h6>Автор</h6>
+                      {selectedCategory === 'author' ? <SelFilterExpand /> : <FilterExpand />}
+                    </div>
+                  </li>
+
+                  <li className={selectedCategory === 'techniqueName' ? 'selected' : ''}
+                    onClick={() => handleFilterCategoryClick('techniqueName')}>
+                    <FilterRelicsLine />
+                    <div className='filter_inner'>
+                      <h6>Техніка</h6>
+                      {selectedCategory === 'techniqueName' ? <SelFilterExpand /> : <FilterExpand />}
+                    </div>
+                    <FilterRelicsLine />
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className='cat_right'>
+            <Search />
+            <div className="cat-items-container">
+              {items && items.map((item, index) => (
+                <Link key={item.id} to={`/catalogue/${item.id}`} className="cat-item">
+                  <img src={item.thumbnailUrl} alt={item.title} />
+                  {/* <div className='cat-item-title'><p>{item.title}</p></div> */}
+                  <div className='cat-item-title'> {index === 0 ? <p>Мозаїчне зображення Димитрія Солунського</p> : <p>{item.title}</p>}</div>
+                </Link>
+              ))}
+            </div>
+
+            <Pagination totalPages={totalPages} currentPage={currentPage} onPageChange={paginate} />
           </div>
         </div>
       </div>
-      <div className='cat_right'>
-        <Search />
-        <div className="cat-items-container">
-          {items && items.map((item, index) => (
-            <Link key={item.id} to={`/catalogue/${item.id}`} className="cat-item">
-              <img src={item.thumbnailUrl} alt={item.title} />
-              {/* <div className='cat-item-title'><a>{item.title}</a></div> */}
-              {index === 0 ? <a>Мозаїчне зображення Димитрія Солунського</a> : <a>{item.title}</a>}
-            </Link>
-          ))}
-        </div>
 
-        <Pagination totalPages={totalPages} currentPage={currentPage} onPageChange={paginate} />
-      </div>
-    </div>
+
   }
   </>
   );
---
+++ src/components/FilterModal/filter_modal.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './filter_modal.css';
+
+interface FilterModalProps {
+  options: string[];
+  selectedOptions: string[];
+  onClose: () => void;
+  onOptionClick: (option: string) => void;
+  style?: React.CSSProperties;
+}
+
+const FilterModal: React.FC<FilterModalProps> = ({
+  options,
+  selectedOptions,
+  onClose,
+  onOptionClick,
+}) => {
+  return (
+    <div className="filter-modal">
+      <div className="filter-options">
+        {options.map((option) => (
+          <div
+            key={option}
+            onClick={() => onOptionClick(option)}
+            className={selectedOptions.includes(option) ? 'selected' : ''}
+          >
+            {option}
+          </div>
+        ))}
+      </div>
+      <div className='filter-btns'>
+        <a onClick={onClose} >Очистити</a>
+        <button onClick={onClose} >Готово</button>
+        </div>
+    </div>
+  );
+};
+
+export default FilterModal;
