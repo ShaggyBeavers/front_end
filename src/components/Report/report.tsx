a6b10393e9c7278a35da27347e3e4a9ea0aa7ce6
yaakymiv <yaryna.yakymiv.shi.2022@lpnu.ua>
2023-12-28 15:20:31 +0200

    report is ready except uploading pics and connecting to real endpoint

--- src/components/Report/report.tsx
+++ src/components/Report/report.tsx
@@ -1,22 +1,30 @@
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { useDropzone } from 'react-dropzone';
 import './report.css';
 import agent from '../../app/api/agent';
+import { useEffect, useState } from 'react';
 
 interface ReportForm {
   name: string,
   category: string,
   location: string,
-  sources: string,
+  sources: string[],
   description: string,
   photos: File[],
 }
 
 export default function Report() {
-  const { register, handleSubmit, formState, getValues, reset } = useForm<ReportForm>();
+  const { register, handleSubmit, formState, reset } = useForm<ReportForm>();
   const { errors } = formState;
+  const [remainingChars, setRemainingChars] = useState(3);
+  const [isTyping, setIsTyping] = useState(false);
+  const [urlInput, setUrlInput] = useState('');
 
   const submitForm: SubmitHandler<ReportForm> = async (data) => {
+
+    data.sources = urlInput.split(' ').filter(url => url.trim() !== '');
     try {
+      console.log(data.sources)
       await agent.Account.report(data);
       reset();
     } catch (error: any) {
@@ -30,19 +38,20 @@ export default function Report() {
     }
   }
 
-  const categories = ["Category 1", "Category 2", "Category 3"];
+  const categories = ["Category 1", "Category 2", "Category 3", "Category 1", "Category 2", "Category 3", "Category 1", "Category 2", "Category 3", "Category 1", "Category 2", "Category 3", "Category 1", "Category 2", "Category 3", "Category 1", "Category 2", "Category 3", "Category 1", "Category 2", "Category 3", "Category 1", "Category 2", "Category 3"];
 
   return (
     <div className='report'>
       <div className='report_con'>
         <div className='report_form'>
           <form onSubmit={handleSubmit(submitForm)} noValidate>
+
             <div className='report_group'>
-              <div className={`report_input ${errors.name ? 'error' : ''}`}>
+              <div className={`report_input ${errors.name ? 'error' : ''}`} style={{ width: '45%' }}>
                 <label htmlFor="name">Назва:</label>
                 <input
                   type="text"
-                  placeholder="Введіть назву"
+                  placeholder="Введіть назву артефакту"
                   {...register('name', {
                     required: 'Заповніть це поле', minLength: {
                       value: 2,
@@ -51,7 +60,8 @@ export default function Report() {
                   })} />
                 <p>{errors.name?.message as string}</p>
               </div>
-              <div className={`report_input ${errors.category ? 'error' : ''}`}>
+
+              <div className={`report_input ${errors.category ? 'error' : ''}`} style={{ width: '45%' }}>
                 <label htmlFor="category">Категорія:</label>
                 <select
                   {...register('category', {
@@ -70,6 +80,62 @@ export default function Report() {
                 <p>{errors.category?.message as string}</p>
               </div>
             </div>
+
+            <div className='report_group'>
+              <div className={`report_input ${errors.location ? 'error' : ''}`} style={{ width: '60%' }}>
+                <label htmlFor="location">Ймовірне місце розташування:<p style={{ color: '#DADADA' }}>(опційно)</p></label>
+                <input
+                  type="text"
+                  placeholder="Введіть місце розташування"
+                  {...register('location', {
+                    minLength: {
+                      value: 2,
+                      message: 'Мінімальна довжина - 2 символи',
+                    },
+                  })} />
+                <p>{errors.location?.message as string}</p>
+              </div>
+
+            {/* HERE GO PICS */}
+
+            </div>
+
+            <div className='report_group'>
+              <div className='report_input' style={{ width: '100%' }}>
+                <label htmlFor="description">Опис:<p style={{ color: '#DADADA' }}>(опційно)</p></label>
+                <textarea
+                  rows={4}
+                  maxLength={300}
+                  placeholder="Введіть опис до 300 символів"
+                  {...register('description')}
+                  onChange={(e) => {
+                    const inputLength = e.target.value.length;
+                    const maxLength = 300;
+                    const remainingChars = maxLength - inputLength;
+                    setRemainingChars(remainingChars);
+                    setIsTyping(true);
+                  }}
+                  onBlur={() => setIsTyping(false)}
+                />
+                {isTyping && <p style={{ fontSize: '11px' }}>Залишилось символів: {remainingChars}</p>}
+              </div>
+            </div>
+
+            <div className='report_group'>
+              <div className={`report_input`} style={{ width: '100%' }}>
+                <label htmlFor="urls">Джерела:<p style={{ color: '#DADADA' }}>(опційно)</p></label>
+                <input
+                  type="text"
+                  placeholder="Введіть посилання через пробіл"
+                  value={urlInput}
+                  onChange={(e) => setUrlInput(e.target.value)}
+                />
+              </div>
+            </div>
+
+
+            <button type="submit">Опублікувати</button>
+
           </form>
         </div>
       </div>
