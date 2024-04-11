9fb86e542392650daa3ae5b3975a6748991d0a78
Bogdan <apelcin4k.2016@gmail.com>
2024-04-11 11:34:39 +0300

    I HEHEHED Safari Users

---
+++ src/components/NotForSafariUser/not_for_safari_user.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './not_for_safari_user.css';
+import Logo404 from '../icons/logo404';
+import { Link } from 'react-router-dom';
+
+
+const NotForSafariUser = () => {
+    const [showBanner, setShowBanner] = useState(true);
+
+    const userAgent = window.navigator.userAgent;
+    const isSafari = /^((?!Chrome|Android).)*Safari/i.test(navigator.userAgent);
+  
+    const handleButtonClick = () => {
+      setShowBanner(false); 
+    };
+  
+    if (showBanner && (isSafari)) {
+      return (
+        <div className="not_for_safari_user_con">
+            <div className="not_for_safari_user">
+                <Logo404 />
+                <h3>Даний веб-сайт може не коректно працювати у вашому браузері.</h3>
+                <button onClick={handleButtonClick}>Все одно зайти</button>
+            </div>
+        </div>
+      );
+    } else {
+        return null; 
+      }
+    };
+
+export default NotForSafariUser;
